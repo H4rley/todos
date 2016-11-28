7a01b7c0e0dc6b00214da185ede4df27cc475182
Viktoria <viktoria.lovasova@gmail.com>
2016-11-28 20:54:53 +0100

    Add current email method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def authenticate
-  if !signed_in?
-    redirect_to new_session_path
+  def authenticate
+    if !signed_in?
+      redirect_to new_session_path
+    end
   end
-end
 
-def signed_in?
-  session[:current_email].present?
-end
+  def signed_in?
+    current_email.present?
+  end
 
+  def current_email
+    session[:current_email]
+  end
 
+  def sign_in_as email
+    session[:current_email] = email
+  end
 end
