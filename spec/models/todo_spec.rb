2b80da65e2916685d6d5238c5a0c76ee16626d1d
Viktoria <viktoria.lovasova@gmail.com>
2016-11-29 22:11:08 +0100

    Refactor completing a toto

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Todo, "#completed?" do
+   it 'returns true if completed at is set' do
+     todo = Todo.new completed_at: Time.current
+     expect(todo).to be_completed
+   end
 
- it 'returns true if completed at is set' do
-   todo = Todo.new completed_at: Time.current
-   expect(todo).to be_completed
- end
-
-it 'returns false if completed at is set' do
-  todo = Todo.new completed_at: nil
-   expect(todo).not_to be_completed
+  it 'returns false if completed at is set' do
+    todo = Todo.new completed_at: nil
+    expect(todo).not_to be_completed
+  end
 end
 
+describe Todo, "#complete!" do
+  it 'update completed_at' do
+    todo = Todo.create! completed_at: nil
+
+    todo.complete!
+
+    todo.reload
+    expect(todo).to be_completed
+  end
 end
